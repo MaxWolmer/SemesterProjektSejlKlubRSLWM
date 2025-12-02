ed714a66dc40b87c376f018d9a961860782f5eb4
Martin Jensen <mwtj1000@gmail.com>
2025-12-02 10:13:09 +0100

    Added summary, comment and Print method

--- ProjektLibrary/Models/Boat.cs
+++ ProjektLibrary/Models/Boat.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace ProjektLibrary.Models
 {
+    /// <summary>
+    /// Represents a boat with an unique identifier
+    /// </summary>
     public class Boat
     {
+        //Martins kommentar
         public int ID { get; set; }
+
+        public void print(string text)
+        {
+            
+        }
     }
+    
 }
