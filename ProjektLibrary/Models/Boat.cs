6570fdaf65243e7eebeaf50537796b3b3b169ad6
Modjo1 <rebeccalauruhn@gmail.com>
2025-12-02 10:19:10 +0100

    lk

--- ProjektLibrary/Models/Boat.cs
+++ ProjektLibrary/Models/Boat.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-///
-/// whatever
-///
+
 
 namespace ProjektLibrary.Models
 {
@@ -14,10 +12,7 @@ namespace ProjektLibrary.Models
     {
         public int ID { get; set; }
 
-        public void PrintBecca(string text)
-        {
-
-        }
+       
 
 
     }
