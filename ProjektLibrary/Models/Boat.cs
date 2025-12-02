8f81736d783f6efae62eb0f6f353ea10f4db1f70
Modjo1 <rebeccalauruhn@gmail.com>
2025-12-02 10:12:08 +0100

    Added Poul Stuff

--- ProjektLibrary/Models/Boat.cs
+++ ProjektLibrary/Models/Boat.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+///
+/// whatever
+///
 
 namespace ProjektLibrary.Models
 {
+    //Id for boat
     public class Boat
     {
         public int ID { get; set; }
+
+        public void PrintBecca(string text)
+        {
+
+        }
+
+
     }
+
+   
 }
