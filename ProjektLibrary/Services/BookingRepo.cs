2022cb53c86610183b01b48770d78440064361e5
William Hjorth Meincke <wim003@edu.zealand.dk>
2025-12-03 15:55:52 +0100

    bookingrepo

--- ProjektLibrary/Services/BookingRepo.cs
+++ ProjektLibrary/Services/BookingRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjektLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace ProjektLibrary.Services
 {
-    public class BookingRepo
+    public class BookingRepository
     {
+        private List<Booking> _bookings;
+
+        public BookingRepository()
+        {
+            _bookings = new List<Booking>();
+        }
+
+        public void addbooking(Booking Abooking)
+        {
+            _bookings.Add(Abooking);
+        }
+        public void deletebooking(Booking Abooking)
+        {
+            _bookings.Remove(Abooking);
+        }
+        public void findbooking(Booking Abooking)
+        {
+            foreach (Booking onebooking in _bookings)
+            {
+                if (Abooking.bookingid == onebooking.bookingid)
+                {
+                    Console.WriteLine(onebooking);
+                }
+            }
+        }
+        public void printallbookings()
+        {
+            foreach (Booking onebooking in _bookings)
+            { Console.WriteLine(onebooking); }
+        }
+
     }
+
 }
