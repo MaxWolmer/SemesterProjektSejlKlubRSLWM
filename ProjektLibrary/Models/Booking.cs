3c60942a8d436c64a52aeaf0b4cbf289bfc166ef
William Hjorth Meincke <wim003@edu.zealand.dk>
2025-12-03 15:26:22 +0100

    test

--- ProjektLibrary/Models/Booking.cs
+++ ProjektLibrary/Models/Booking.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace ProjektLibrary.Models
 {
-    internal class Booking
+    public class Booking
     {
+        private DateTime _thetime;
+        private User _theuser;
+        private Boat _aBoat;
+        private static int _bookingID = 0;
+        private bool _bookingdone = false;
+
+        public Booking(User theuser, Boat Aboat, int bookingdate, int bookinghourstart, int bookinghourend)
+        {
+
+            _aBoat = Aboat;
+            _theuser = theuser;
+            _thetime = new DateTime();
+            _bookingID++;
+
+
+        }
+        public int bookingid
+        { get { return _bookingID; } }
+
+        public bool bookingdone
+        {
+            get { return _bookingdone; }
+            set { _bookingdone = value; }
+        }
+        public User Theuser
+        {
+            get { return _theuser; }
+            set { _theuser = value; }
+        }
+        public Boat Theboat
+        {
+            get { return _aBoat; }
+            set { _aBoat = value; }
+        }
+        public int bookings
+        {
+            get { return _bookingID; }
+            set { _bookingID = value; }
+        }
+
+        public void Bookingavaible()
     }
 }
