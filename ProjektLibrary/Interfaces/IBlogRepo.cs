8fa4f6523204e898888501d259684a2c185f6085
Lukas Asmussen <lua004@edu.zealand.dk>
2025-12-04 13:02:23 +0100

    Har ændret navn på Blog Model & service. Har tilføjet en body til Blogpost Objekt

--- ProjektLibrary/Interfaces/IBlogRepo.cs
+++ ProjektLibrary/Interfaces/IBlogRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjektLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace ProjektLibrary.Interfaces
 {
-    internal interface IBlogRepo
+    public interface IBlogRepo
     {
+        Blogpost AddTitle();
+        Blogpost AddDescription();
     }
 }
