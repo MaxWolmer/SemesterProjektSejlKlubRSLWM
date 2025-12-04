59f67b9f0b6332791e517e67bac1fbee842d84a4
Martin Jensen <mwtj1000@gmail.com>
2025-12-04 09:55:56 +0100

    d

--- ProjektLibrary/Interfaces/IBoatRepo.cs
+++ ProjektLibrary/Interfaces/IBoatRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjektLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace ProjektLibrary.Interfaces
 {
-    internal interface IBoatRepo
+    public interface IBoatRepo
     {
+
+        void AddBoat(Boat boat);
+        void DeleteBoat(string name);
+        void FindBoatByName(string name);
+        void PrintAllBoats();
     }
 }
