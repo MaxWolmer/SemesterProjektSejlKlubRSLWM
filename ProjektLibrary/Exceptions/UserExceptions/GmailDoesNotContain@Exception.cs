b3a3e1688b756ed6c76a1b3a8b51503c9de36cab
Modjo1 <rebeccalauruhn@gmail.com>
2025-12-08 13:53:17 +0100

    Har Lavet lidt videre på mit User KLasser

---
+++ ProjektLibrary/Exceptions/UserExceptions/GmailDoesNotContain@Exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektLibrary.Exceptions.UserExceptions
+{
+    public class GmailDoesNotContain_Exception : Exception
+    {
+        public GmailDoesNotContain_Exception(string message) : base(message)
+        {
+
+        }
+    }
+}
