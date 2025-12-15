519688146052426319bd9ffe965fee371496f2ef
Martin Jensen <mwtj1000@gmail.com>
2025-12-15 10:07:49 +0100

    Rettede merge konflikt. Williams version i BoatName property er den endelige

--- ProjektLibrary/Models/Boat.cs
+++ ProjektLibrary/Models/Boat.cs
@@ -26,8 +26,7 @@ namespace ProjektLibrary.Models
         }
         
         public string BoatName { get { return _name; } set { if (_admin) { { _name = value; } } } } 
-        public string BoatName { get { return _name; }
-            set { _name = value; } }
+
 
         public string BoatModel { get { return _model; } set { if (_admin) { { _model = value; } } } }
 
