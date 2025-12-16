96377d1a4a5ddaf058f6e45083a63b5d94a27cbc
SofieWT <sot004@edu.zealand.dk>
2025-12-16 09:26:52 +0100

    Rettede i egen program

--- Sofie Console/Program.cs
+++ Sofie Console/Program.cs
@@ -7,9 +7,11 @@ using ProjektLibrary.Services;
 RepairRepo rRepo = new RepairRepo();
 
 #region Ekstra Data
-Boat b1 = new Boat("Svane","FEVA", true);
-Boat b2 = new Boat("Havfruen", "FEVA", true);
 User u1 = new User("Marlene", "Marlene@mail.com", "10101010", false);
+User u2 = new User("Jytte","Jytte@mail.com", "20202020", true);
+
+Boat b1 = new Boat("Svane", "FEVA",u1);
+Boat b2 = new Boat("Kiwi", "Fishermans", u2);
 
 Repair r5 = new Repair("Malingen er skallet af bagbordside", false, b1, u1);
 Repair r6 = new Repair("Flænge i sejlet", false, b1, u1);
@@ -69,8 +71,18 @@ Console.WriteLine();
 #region Test af RemoveRepairById(int id)
 //Console.WriteLine("Testing RemoveRepairById:");
 //Console.WriteLine($"Der er {rRepo.Count} antal reperationer før remove.");
-
-//rRepo.RemoveRepairById(1);
+//try
+//{
+//    rRepo.RemoveRepairById(1);
+//}
+//catch (RepairIdDoesNotExistException idex)
+//{
+//    Console.WriteLine($"Fejlbesked: {idex.Message}");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"Fejlbesked: {ex.Message}");
+//}
 
 //Console.WriteLine($"Der er {rRepo.Count} antal reperationer efter remove.");
 #endregion
@@ -126,18 +138,18 @@ Console.WriteLine();
 #endregion
 
 #region Test af GetRepairsInBoat(int boatId)
-Console.WriteLine("Test af GetRepairsInBoat:");
-rRepo.GetRepairsForBoat(b1.BoatId);
+//Console.WriteLine("Test af GetRepairsInBoat:");
+//rRepo.GetRepairsForBoat(b1.BoatId);
 
-foreach (Repair r in rRepo.GetRepairsForBoat(b1.BoatId))
-{
-    Console.WriteLine(r.ToString());
-}
+//foreach (Repair r in rRepo.GetRepairsForBoat(b1.BoatId))
+//{
+//    Console.WriteLine(r.ToString());
+//}
 
-Console.WriteLine("Test med båd uden reparationer:");
-foreach (Repair r in rRepo.GetRepairsForBoat(b2.BoatId))
-{
-    Console.WriteLine(r);
-}
+//Console.WriteLine("Test med båd uden reparationer:");
+//foreach (Repair r in rRepo.GetRepairsForBoat(b2.BoatId))
+//{
+//    Console.WriteLine(r);
+//}
 
 #endregion
